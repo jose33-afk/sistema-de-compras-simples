22deec1487982f5af5a4e5f29ba8186c93b002f7
jose33-afk <josedev337@gmail.com>
2025-09-12 18:37:18 -0300

    funcoes novas
    mostrar mercado feito, e tratamento de erros

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { readFile } from "node:fs/promises";
 import readline from "node:readline/promises"; //Esse funciona com await.
 import { stdin as input, stdout as output } from "node:process";
+import chalk from "chalk";
 
 const menu = () => {
   output.write(`${"=".repeat(28)}\n`);
@@ -26,10 +27,19 @@ async function principal() {
       if (opcSelec === "5") {
         rl.close();
         break;
-      } else if (opcSelec === "1") verProdutos();
+      } else if (opcSelec === "1") {
+        verProdutos();
+      }
       else if (opcSelec === "2") {
         addProduto();
         break;
+      } else if (opcSelec === "3") {
+        if (carrinho.length > 0) {
+          mostrarCarrinho();
+          break;
+        } else {
+          console.log(chalk.red("Voce ainda n adicionou Produtos ao Carrinho!"));
+        }
       }
     }
   }
@@ -38,7 +48,7 @@ async function principal() {
     output.write(`${"=".repeat(28)}\n`);
     dados.produtos.forEach((produto) => {
       output.write(
-        `[${produto.id}] - ${produto.produto}, preco:${produto.preco}, estoque:${produto.quantidade}\n`
+        chalk.cyan(`[${produto.id}] - ${produto.produto}, preco:${produto.preco}, estoque:${produto.quantidade}\n`)
       );
     });
   }
@@ -53,32 +63,72 @@ async function principal() {
 
     while (true) {
       opc = await rl.question("Numero do produto:");
-      if (opc >= 1 && opc <= 3) {
+      if (opc >= 1 && opc <= quantidadeProduts) {
+        let estoqueStatus = "Adicionando ao Carrinho..";
         let produtoAchado = dados.produtos.find(produto => produto.id === opc);
-        carrinho.push({nome: produtoAchado.produto, preco: produtoAchado.preco})
-        console.log(produtoAchado.preco)
-        console.log(`Carrinho ${carrinho[0].nome} preco:${carrinho[0].preco}`)
-        //chamar reset, falta modificar a quantidade do estoque olha chat gpt, e falta criar a interface pra carrinho.
+        let carrinhoEncontrado = carrinho.find(item => item.nome === produtoAchado.produto);
+
+        if (carrinhoEncontrado) { //4
+          if (carrinhoEncontrado.quantidade < produtoAchado.quantidade) carrinhoEncontrado.quantidade += 1;
+          else estoqueStatus = "Acabou o Estoque de";
+        }
+        else {
+          //Aqui por sistema pra mudar o json produtos.
+
+
+          carrinho.push({nome: produtoAchado.produto, preco: produtoAchado.preco, quantidade:1});
+        }
+        console.log(chalk.yellow(`${estoqueStatus} ${produtoAchado.produto}`))
+        delay(interfaceMercado, 1000) 
         break;
+
       } else {
-        console.log("Selecione um produto EXISTENTE!\nLista de produtos");
+        console.log(chalk.red("Selecione um produto EXISTENTE!\nLista de produtos"));
         verProdutos();
       }
-      console.log(`Opcao Digitada:${opc}`);
     }
   }
 
-  function reseta() {
-    rl.close();
-    interfaceMercado();
+  function mostrarCarrinho () {
+    output.write(`${"=".repeat(28)}\n`);
+    console.log(chalk.yellow("Carrinho:"));
+    for (let item of carrinho) {
+      console.log(chalk.yellow(`Produto:${item.nome},Valor ${item.preco}, Quantidade:${item.quantidade}`))
+    }
+    delay(interfaceMercado, 1000) 
   }
 }
 
+function delay(fun, ms) {
+  output.write(chalk.green("carregando...\n"))
+  setTimeout(fun, ms);
+} 
+
 //Inicio
-//output.write("Mercado\n");
+output.write(chalk.magenta("Mercado\n"));
 principal();
 
 /*
  1 - readfile so entende txt, por isso converte primeiro, pra depois transformar um arquivo txt em forma de json em obj.
  2 - Tem que ficar aqui, se ele ficar fora ele fica funcionando e n finaliza o programa msm n chamando a funcao principal.
+ 3 - Tem um segundo parametro pra caso der erro new Promise((resolve, reject) => {calback});
+    function lerArquivo(nome) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(nome, (err, data) => {
+            if (err) {
+                reject(err); // Deu erro
+            } else {
+                resolve(data); // Deu certo
+            }
+        });
+    });
+
+    SetTimeout so funcina uma vez, e resolve e como se fosse a funcao, lembra setTimeOut(calback, delay);
+  }
+
+  4 - Objetos: truthy ✅
+      Arrays: truthy ✅
+      Funções: truthy ✅
+      null: falsy ❌ (única exceção "objeto")
+
 */
